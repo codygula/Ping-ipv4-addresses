5fecaeb684e332a49d03e728f5e3264d4d967327
Cody Gula <codygula@yahoo.com>
2022-06-29 15:03:12 -0700

    script now prints to a file in the same directory instead of just to the console.

--- Print every ipv4 address.py
+++ Print every ipv4 address.py
@@ -2,12 +2,33 @@
 # TODO: make it fill in a Hilbert Curve to create a map of the entire IPV4 internet.
 
 import subprocess
+import time
 
 # ping -i sets interval between each ping, -c sets number of pings
 def ping(ipaddress):
-    out = subprocess.run("ping -c 1 " + ipaddress, shell=True)
+    out = subprocess.run("ping -c 1 " + ipaddress, capture_output=True, shell=True)
     return out
     
+
+# create and open a new txt file
+subprocess.run("cd /home", capture_output=True, shell=True)
+document1 = subprocess.run("touch pingTest.txt", shell=True) # + time.strftime("%H:%M:%S") +".txt", shell=True)
+print(document1)
+
+e = open("pingTest.txt", "a")
+e.write("######  ### #     #  #####  \n")
+e.write("#     #  #  ##    # #     # \n")
+e.write("#     #  #  # #   # #       \n")
+e.write("######   #  #  #  # #  #### \n")
+e.write("#        #  #   # # #     # \n")
+e.write("#        #  #    ## #     # \n")
+e.write("#       ### #     #  #####  \n \n")
+e.write("Pinging IP addresses in range " + "0.0.0.0" + " to " + "255.255.255.255" + "\n")
+e.write("Started at " + str(time.time()) +"\n")
+e.write('\n')
+e.close()
+
+
 address1 = 0
 address2 = 0
 address3 = 0
@@ -21,7 +42,11 @@ while address4 <= 255:
             while address1 <= 255:
                 finalAddress = str(address4) + "." + str(address3) + "." + str(address2) + "." + str(address1)
                 print("now pinging " + finalAddress)
-                print(ping(finalAddress))
+                z = ping(finalAddress)
+                f = open("pingTest.txt", "a")
+                f.write("######################################## ")
+                f.write(z.stdout.decode())
+                f.close()
                 address1 = int(address1) + 1
             address2 += 1
             address1 = 0
